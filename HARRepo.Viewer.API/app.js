047a90a5adea66a41da60c6b3f455c0f2175f6d3
Pavel Nedkov <pnedkov.usgov@gmail.com>
2021-02-16 22:43:40 +0200

    ViewerAPI fixed version

--- HARRepo.Viewer.API/app.js
+++ HARRepo.Viewer.API/app.js
@@ -30,12 +30,7 @@ loadSettings().then(() => {
         algorithms: ['RS256']
     });
 
-    var corsOptions = {
-        origin: settings.AllowedOrigin,
-        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-    }
-
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use(jwtCheck);
 
     app.get('/HAR', async (req, res) => {
